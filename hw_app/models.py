89ef5a50f12ac98740df4de1372d10a00e449b84
benhigh22 <benjaming.high@gmail.com>
2016-02-24 17:47:23 -0500

    Good but could be better

--- hw_app/models.py
+++ hw_app/models.py
@@ -7,7 +7,8 @@ class FantasyStat(models.Model):
     name = models.CharField(max_length=50)
     position = models.CharField(max_length=5)
     points = models.FloatField()
-    
+    weekly_points = models.FloatField()
+
 
 
     def __str__(self):
