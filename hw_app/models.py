f634a83e9fa04fe16d0c2ff3e8acb938d2e8999a
benhigh22 <benjaming.high@gmail.com>
2016-02-24 16:36:43 -0500

    Next commit

--- hw_app/models.py
+++ hw_app/models.py
@@ -7,6 +7,8 @@ class FantasyStat(models.Model):
     name = models.CharField(max_length=50)
     position = models.CharField(max_length=5)
     points = models.FloatField()
+    
+
 
     def __str__(self):
         return self.name
